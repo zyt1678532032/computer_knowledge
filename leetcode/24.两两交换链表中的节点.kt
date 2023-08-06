a96b28d602b462dd74be8f3b00406cbb8c47eb77
Yantong_zhang <1678532032@qq.com>
2023-08-06 12:32:28 +0800

    feat:24.两两交换链表中的节点 label:链表、递归

---
+++ leetcode/24.两两交换链表中的节点.kt
@@ -0,0 +1,44 @@
+import leetcode.util.ListNode
+
+fun main() {
+    val data = listOf(1, 2, 3, 4)
+    val head = createListNode(data)
+    swapPairs(head)
+}
+
+fun swapPairs(head: ListNode?): ListNode? {
+    var cache = -1
+    var p = head
+    var pre = p
+    while (p != null) {
+        if (cache == -1) {
+            cache = p.`val`
+            pre = p
+            p = p.next
+            continue
+        }
+        pre?.`val` = p.`val`
+        p.`val` = cache
+        cache = -1
+        pre = p.next
+        p = pre
+    }
+    return head
+}
+
+fun createListNode(data: List<Int>): ListNode? {
+    var head: ListNode? = null
+    var help: ListNode? = null
+    if (data.isNotEmpty()) {
+        data.forEach {
+            if (head == null) {
+                head = ListNode(it)
+                help = head
+                return@forEach
+            }
+            help?.next = ListNode(it)
+            help = help?.next
+        }
+    }
+    return head
+}
---
+++ leetcode/util/ListNode.kt
@@ -0,0 +1,5 @@
+package leetcode.util
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
